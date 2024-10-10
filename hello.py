3d480ee57cd04f8eb1ad14e0579d76ce9ecaa96c
Guzman <francisco.guzman@accenture.com>
2024-10-10 10:10:41 -0400

    python first program

---
+++ hello.py
@@ -0,0 +1,8 @@
+
+# print("This is file 2")
+# print(100)
+name = input("What's your name? ")
+age = input("How old are you? ")
+
+print("Hello, " + name + "!")
+print("You are " + age + " years old.")
